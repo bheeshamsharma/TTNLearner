eb9c4cb906ab89c36e892cbb735e9fe464a14d61
Neelesh mishra <neelesh.mishra@shoplc.com>
2022-05-07 07:05:02 +0530

    First code deployed

--- app/src/main/java/com/psgpw/geek_ttn/ui/MainActivity.kt
+++ app/src/main/java/com/psgpw/geek_ttn/ui/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.psgpw.geek_ttn
+package com.psgpw.geek_ttn.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -6,6 +6,7 @@ import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import androidx.navigation.ui.NavigationUI
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.psgpw.geek_ttn.R
 
 class MainActivity : AppCompatActivity() {
 
---
+++ app/src/main/java/com/psgpw/geek_ttn/ui/SplashActivity.kt
@@ -0,0 +1,23 @@
+package com.psgpw.geek_ttn.ui
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.psgpw.geek_ttn.R
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        GlobalScope.launch {
+            delay(2000L)
+            val intent:Intent=Intent(this@SplashActivity, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+}
