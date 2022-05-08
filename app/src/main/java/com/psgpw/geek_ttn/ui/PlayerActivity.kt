b88c69e9162564ca05073353b72bdfddaea7d0a9
bheesham.sharma <bheesham.sharma928@gmail.com>
2022-05-08 09:50:30 +0530

    Design Player activity

--- app/src/main/java/com/psgpw/geek_ttn/ui/PlayerActivity.kt
+++ app/src/main/java/com/psgpw/geek_ttn/ui/PlayerActivity.kt
@@ -1,13 +1,26 @@
-
 package com.psgpw.geek_ttn.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.MediaController
 import com.psgpw.geek_ttn.R
+import com.psgpw.geek_ttn.databinding.ActivityPlayerBinding
 
 class PlayerActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityPlayerBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_player)
+        binding = ActivityPlayerBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.videoPlayer.setOnPreparedListener {
+            it.setOnVideoSizeChangedListener { mp, width, height ->
+                val mediaController = MediaController(this)
+                binding.videoPlayer.setMediaController(mediaController)
+                mediaController.setAnchorView(binding.videoPlayer)
+            }
+        }
+        binding.videoPlayer.setVideoPath("https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerJoyrides.mp4")
+        binding.videoPlayer.start()
     }
 }
